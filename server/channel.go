69c3080af4ee8264f755bb771a9cd0ffcae78ec9
Marduk <dr.marduk@gmail.com>
2014-12-29 20:18:13 +0100

    Added a basic Channelsystem with Users.

---
+++ server/channel.go
@@ -0,0 +1,51 @@
+// channel
+package main
+
+import (
+	"errors"
+	"net"
+	"time"
+
+	"golang.org/x/crypto/otr"
+)
+
+type Channel struct {
+	Name  string
+	Topic string
+	Users []User
+}
+
+type User struct {
+	Username   string
+	PublicKey  otr.PublicKey
+	Connection net.Conn
+	JoinTime   time.Time
+}
+
+func (c *Channel) AddUser(name string, key otr.PublicKey, c net.Conn) error {
+	u := User{
+		Username:   name,
+		PublicKey:  key,
+		Connection: c,
+		JoinTime:   time.Now,
+	}
+
+	c.Users = append(c.Users, u)
+	return nil
+}
+
+func (c *Channel) RemoveUser(name string) error {
+	for i, u := range c.Users {
+
+		if u.Username == name {
+			c.Users = append(c.Users[:i], c.Users[i+1:]...)
+			return nil
+		}
+	}
+
+	return errors.New("User not found in Userlist.")
+}
+
+// This function
+func (c *Channel) SyncUserlist() {
+}
